366d62d423267730017dd1203c417dc2061ef125
emman <emmanuel240158@gmail.com>
2026-03-04 22:23:20 -0500

    feat:service. -ProductDescriptionService. -index.html

---
+++ src/main/kotlin/com/floristeriaakasia/backend/feature/productDescription/ProductDescriptionService.kt
@@ -0,0 +1,13 @@
+package com.floristeriaakasia.backend.feature.productDescription
+
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class ProductDescriptionService(){
+
+    @Transactional
+    fun create(){
+
+    }
+}
